f6d4c3017ff414d1befd115663102822c20de88f
vernonthedev <techjaja2@gmail.com>
2024-10-16 07:52:02 -0700

    [+]Update: Defined generate qrcode func and struct.

--- main.go
+++ main.go
@@ -5,6 +5,23 @@ import (
     "fmt"
 )
 
+//define the qrcode properties using a struct
+type simpleQrcode struct{
+    Content string
+    Size    int
+}
+
+func (code *simpleQrcode) generate() ([]byte,error){
+    // generate qrcode using go's inbuilt qrcode lib using size and the content frm the simpleQrcode struct
+    qrCode, err := qrcode.Encode(code.Content, qrcode.Medium, code.Size)
+    //incase we have any errors as we generate the qrcode then show the errors.
+    if err != nil {
+        return nil, fmt.Errorf("Could not generate qrcode: %v", err)
+    }
+    return qrCode, nil
+}
+
+
 //handle what ever request is sent under the generate route
 func handleRequest(writer http.ResponseWriter, request *http.Request) {
     
